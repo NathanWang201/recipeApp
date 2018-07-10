39fdc80e6acef7e4936e9023fd6a767f5ddd4f5f
NathanWang <nathanwang201@hotmail.com>
2018-07-10 16:51:59 +0930

    Add command object for mapping form post
    Expose command object to web tier instead of domain object in case the gap between form object and domain become larger in the future.

---
+++ src/main/java/com/hao/recipeApp/commands/CategoryCommand.java
@@ -0,0 +1,14 @@
+package com.hao.recipeApp.commands;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryCommand {
+    private long id;
+    private String description;
+}
---
+++ src/main/java/com/hao/recipeApp/converters/CategoryCommandToCategory.java
@@ -0,0 +1,5 @@
+package com.hao.recipeApp.converters;
+
+public class CategoryCommandToCategory {
+
+}
